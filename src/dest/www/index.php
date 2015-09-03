b374d31c91b6b7c31e9bc5267a4140243751e6bd
Ricardo Padilha <ricardospadilha@gmail.com>
2015-09-03 09:51:59 -0700

    Improved configuration page, added troubleshooting section.

--- src/dest/www/index.php
+++ src/dest/www/index.php
@@ -9,6 +9,12 @@ $appsite = "http://modoboa.org/";
 $apppage = "http://".$_SERVER['SERVER_ADDR'].":8000/";
 $apphelp = "http://modoboa.org/en/support/";
 
+$appret = shell_exec("/usr/bin/DroboApps.sh status_app mailserver");
+$apprunning = strpos ($appret, "running");
+$publicip = shell_exec("/usr/bin/wget -qO- http://ipecho.net/plain");
+$blacklistsite = "http://mxtoolbox.com/SuperTool.aspx?action=blacklist%3a".$publicip."&run=toolpage";
+$mxsite = "http://mxtoolbox.com/";
+$dnssite = "https://toolbox.googleapps.com/apps/checkmx/";
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -100,6 +106,7 @@ $apphelp = "http://modoboa.org/en/support/";
             </div>
           </form>
           <p>Please change the password as soon as possible.</p>
+          <p>Once logged in, you can proceed to creating your email domain and accounts. Keep in mind that the server admin account is not capable of sending and receiving email, so log in with a user account and click &quot;Webmail&quot; on the top left to send and receive email.</p>
         </div>
       </div>
     </div>
@@ -115,14 +122,35 @@ $apphelp = "http://modoboa.org/en/support/";
         <div class="panel-body">
           <p>A few extra steps are necessary to make the Drobo your main email server. Those are:</p>
           <ol>
-            <li>Make sure your Drobo is reachable from the internet. The following <a href="https://en.wikipedia.org/wiki/List_of_TCP_and_UDP_port_numbers" target="_new">ports</a> must be reachable from the internet (check your <a href="http://portforward.com/" target="_new">router and/or firewall documentation</a>):</li>
+            <li>Make sure your Drobo is reachable from the internet. The following <a href="https://en.wikipedia.org/wiki/List_of_TCP_and_UDP_port_numbers" target="_new">ports</a> must be reachable from the internet (check your <a href="http://portforward.com/" target="_new">router and/or firewall documentation</a>), and must be forwarded to the Drobo:</li>
               <ul>
                 <li>Postfix: TCP:25 and TCP:465</li>
                 <li>Dovecot: TCP:110, TCP:143, TCP:993, and TCP:995</li>
                 <li>Modoboa: TCP:8000</li>
               </ul>
-            <li>Configure the DNS <a href="https://support.google.com/a/answer/140034?hl=en" target="_new">MX records</a> on your domain to point to your Drobo.</li>
+            <li>Configure the DNS <a href="https://support.google.com/a/answer/140034?hl=en" target="_new">MX records</a> on your domain to point to your public IP address.</li>
           </ol>
+          <p>Once the DNS records have been updated (it may take a couple of hours), you can log in with a user account to send and receive email.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- troubleshooting -->
+  <div class="panel-group" id="troubleshooting">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h4 class="panel-title"><a data-toggle="collapse" data-parent="#troubleshooting" href="#troubleshootingbody">Troubleshooting</a></h4>
+      </div>
+      <div id="troubleshootingbody" class="panel-collapse collapse">
+        <div class="panel-body">
+          <p><strong>Error: &quot;NoReverseMatch at ... Reverse for '...' with arguments '...' and keyword arguments '...' not found.&quot;</strong></p>
+          <p>Please restart mailserver. This is happening because a new extension was enabled, which requires a server restart.</p>
+          <p><strong>I cannot send email.</strong></p>
+          <p>Make sure that your public IP address is not <a href="<?php echo $blacklistsite; ?>" target="_new">blacklisted</a>.</p>
+          <p><strong>I cannot receive email.</strong></p>
+          <?php if (! $appstatus) { ?><p>Make sure that mailserver is running. Currently it seems to be <strong>stopped</strong>.</p><?php } ?>
+          <p>Make sure that the MX records for your domain have been <a href="<?php echo $mxsite; ?>" target="_new">propagated</a>. You can also try a more comprehensive <a href="<?php echo $dnssite; ?>" target="_new">DNS check</a>.</p>
         </div>
       </div>
     </div>
@@ -134,7 +162,7 @@ $apphelp = "http://modoboa.org/en/support/";
       <div class="panel-heading">
         <h4 class="panel-title"><a data-toggle="collapse" data-parent="#summary" href="#summarybody">Summary of changes</a></h4>
       </div>
-      <div id="summarybody" class="panel-collapse collapse in">
+      <div id="summarybody" class="panel-collapse collapse">
         <div class="panel-body">
           <p>Changes from 1.0.0:</p>
           <ol>
